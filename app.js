274b37716bea74ab50d032aeeb003d499fcde8f1
Ratu Mutiara <rmpapp8@gmail.com>
2021-08-15 22:46:08 +0700

    update150821

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use('/',router)
+app.use(express.static('public')) 
 
 app.listen(port,()=>{
     console.log(`App running on port ${port}`)
